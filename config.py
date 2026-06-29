1c8e281f1105452039f1fd9c47f8d5909de95477
sbtembar8as-rgb <sbtembar8as@gmail.com>
2026-06-30 02:15:51 +0300

    Update configuration values in config.py
    Updated BOT_TOKEN, BOT_NAME, API_ID, and API_HASH with new values.

--- config.py
+++ config.py
@@ -5,11 +5,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 SESSION_NAME = getenv("SESSION_NAME", "session")
-BOT_TOKEN = getenv("BOT_TOKEN")
-BOT_NAME = getenv("BOT_NAME")
+BOT_TOKEN = getenv("8709552170:AAELuLrOYtjCe02IK_Nv3fndv_yl10gSqqg")
+BOT_NAME = getenv("ميوزك")
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("38841029"))
+API_HASH = getenv("289dc5614e276b770dfc5718fcd9248d")
 
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "7"))
 
